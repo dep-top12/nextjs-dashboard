03014e628fd3c5c4ee9957ee323b89b016122b3d
dep-top12 <deptop725+12@gmail.com>
2024-02-18 17:02:00 +0900

    finish'

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,17 @@
-// import Pagination from "@/app/ui/invoices/pagination";
+import { fetchInvoicesPages } from '@/app/lib/data';
 import { inter } from '@/app/ui/fonts';
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
 import Table from '@/app/ui/invoices/table';
 import Search from "@/app/ui/search";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from 'next';
 import { Suspense } from "react";
  
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
+
 export default async function Page({
     searchParams,
 } : {
@@ -16,6 +22,7 @@ export default async function Page({
 }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -32,7 +39,7 @@ export default async function Page({
                     />
                 </Suspense>
                 <div className="mt-5 flex w-full justify-center">
-                    {/* <Pagination totalPages={totalPages}/> */}
+                    <Pagination totalPages={totalPages}/>
                 </div>
             </div>
         </div>
